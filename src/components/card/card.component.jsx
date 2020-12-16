3990924cdc40864e9ec93c26e7bc176bdd2b32e7
yaonur <ya_onur@hotmail.com>
2020-12-16 18:34:31 +0300

    edit2

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,7 +7,7 @@ export const Card = (props) => (
       alt="monsters"
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     />
-    <h1>{props.monster.name}</h1>
+    <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
   </div>
 );
